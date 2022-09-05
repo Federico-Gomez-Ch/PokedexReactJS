267dc052d3ce8259286b5615f8d2b53222da5215
federicogomez016 <federicogomez016@outlook.com>
2022-09-05 00:06:08 -0300

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,9 +3,8 @@ import './Footer.css'
 const Footer = () => (
 <footer>
     <div>
-    <p>Copyright © 2022 ConversorDeMonedas</p>
-
     <p>Power by Federico Gomez Ch</p>
+    <p>Copyright © 2022</p>
     </div>
     <div>
     <p>Datos en tiempo real gracias a:
